3046607b30100cfdb0badde439adf17115ca8632
ivantomicic-crowdhouse <ivan@tomicic.dev>
2024-12-21 14:30:17 +0100

    removed stuff

--- netlify/functions/webhook.ts
+++ netlify/functions/webhook.ts
@@ -1,38 +1,10 @@
-import type { Handler } from "@netlify/functions";
+import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event) => {
-	// Log incoming request for debugging
-	console.log({
+	console.log("Incoming request:", {
 		path: event.path,
 		method: event.httpMethod,
-		headers: event.headers,
 	});
-
-	// Handle CORS preflight
-	if (event.httpMethod === "OPTIONS") {
-		return {
-			statusCode: 204,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Headers": "Content-Type",
-				"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-			},
-		};
-	}
-
-	// Only allow POST
-	if (event.httpMethod !== "POST") {
-		return {
-			statusCode: 405,
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				error: "Method not allowed",
-				allowedMethods: ["POST"],
-			}),
-		};
-	}
 
 	try {
 		const buttonId = event.path.split("/").pop();
